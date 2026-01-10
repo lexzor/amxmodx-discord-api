405998440a0447fa208d7834db81393ca77b4196
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2026-01-10 11:34:32 +0200

    Added some comments to h_export.cpp

--- src/metamod/h_export.cpp
+++ src/metamod/h_export.cpp
@@ -1,8 +1,11 @@
 #include "precompiled.h"
 
-enginefuncs_t g_engfuncs;
-globalvars_t* gpGlobals;
+enginefuncs_t g_engfuncs;	// engine funcs
+globalvars_t* gpGlobals;	// global variables
 
+// Receive engine function table from engine.
+// This appears to be the _first_ DLL routine called by the engine, so we
+// do some setup operations here.
 C_DLLEXPORT void WINAPI GiveFnptrsToDll(enginefuncs_t* pengfuncsFromEngine, globalvars_t* pGlobals)
 {
 	memcpy(&g_engfuncs, pengfuncsFromEngine, sizeof(enginefuncs_t));
