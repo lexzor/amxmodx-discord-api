4802bbd3d1bde04db1486f8f2808fbab59ce7cb8
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2026-04-06 15:30:56 +0300

    perf: added release build mode for lower file size

--- docker-images/debian12/build_project.ps1
+++ docker-images/debian12/build_project.ps1
@@ -1,12 +1,11 @@
+param(
+    [switch]$debug
+)
+
+$buildType = if ($debug) { "Debug" } else { "Release" }
+
 docker run -it --rm `
     -v "${PWD}/../..:/workspace" `
     -w /workspace `
     debian12-i386 `
-  bash -c "\
-    rm -rf build && \
-    mkdir build && \
-    cd build && \
-    cmake .. && \
-    make -j$nproc && \
-    echo 'Press any key to exit the docker container' && \
-    read -n 1"
+    bash -c "rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=$buildType && make -j`$(nproc) && echo 'Build complete ($buildType mode). Press any key to exit.' && read -n 1"
