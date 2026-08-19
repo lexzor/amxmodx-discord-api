f04dc45dfdacd7bbba4519adfe3bc1a63f14499d
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2026-08-19 14:26:48 +0300

    Update build_project.ps1

--- docker-images/debian12-i386/build_project.ps1
+++ docker-images/debian12-i386/build_project.ps1
@@ -8,4 +8,4 @@ docker run -it --rm `
     -v "${PWD}/../..:/workspace" `
     -w /workspace `
     debian12-i386 `
-    bash -c "rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=$buildType && cores=`$(nproc); if [ `$cores -lt 2 ]; then cores=2; fi && make -j`$cores && echo 'Build complete ($buildType mode). Press any key to exit.' && read -n 1"
+        bash -c "rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=$buildType && cores=`$(( `$(nproc) / 2 )); if [ `$cores -lt 1 ]; then cores=1; fi && make -j`$cores && echo 'Build complete ($buildType mode). Press any key to exit.' && read -n 1"
