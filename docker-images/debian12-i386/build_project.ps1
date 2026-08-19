a9a8954b813fbe51d260b366d051ef8c9e426087
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2026-08-19 13:10:46 +0300

    feat: build_project.ps1 should not use at least 2 cores

--- docker-images/debian12-i386/build_project.ps1
+++ docker-images/debian12-i386/build_project.ps1
@@ -8,4 +8,4 @@ docker run -it --rm `
     -v "${PWD}/../..:/workspace" `
     -w /workspace `
     debian12-i386 `
-    bash -c "rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=$buildType && make -j`$(nproc) && echo 'Build complete ($buildType mode). Press any key to exit.' && read -n 1"
+    bash -c "rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=$buildType && cores=`$(nproc); if [ `$cores -lt 2 ]; then cores=2; fi && make -j`$cores && echo 'Build complete ($buildType mode). Press any key to exit.' && read -n 1"
