87a7890a93fbe7c57c89c9dde256f8d588ecce71
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2026-08-20 14:31:58 +0300

    feat: register new added forwards into AMXX at runtime

--- src/amxx/amx_forwards.cpp
+++ src/amxx/amx_forwards.cpp
@@ -16,6 +16,14 @@ const char* g_ForwardsNames[] = {
 	{ "OnGuildSlashCommandDelete" },
 	{ "OnGuildSlashCommand" },
 
+	{ "OnGuildMemberCreate" },
+	{ "OnGuildMemberDelete" },
+	{ "OnGuildMemberUpdate" },
+	{ "OnGuildMemberFetch" },
+
+	{ "OnGuildMemberRoleAdd" },
+	{ "OnGuildMemberRoleRemove" },
+
 	{ "OnChannelMessageCreated" },
 
 };
@@ -37,6 +45,14 @@ void RegisterForwards()
 	g_Forwards[ON_GUILD_SLASH_COMMAND_CREATE]		= MF_RegisterForward(g_ForwardsNames[ON_GUILD_SLASH_COMMAND_CREATE], ET_IGNORE, FP_STRING, FP_CELL, FP_STRING, FP_STRING, FP_DONE);
 	g_Forwards[ON_GUILD_SLASH_COMMAND_DELETE]		= MF_RegisterForward(g_ForwardsNames[ON_GUILD_SLASH_COMMAND_DELETE], ET_IGNORE, FP_STRING, FP_CELL, FP_STRING, FP_STRING, FP_DONE);
 	g_Forwards[ON_GUILD_SLASH_COMMAND]				= MF_RegisterForward(g_ForwardsNames[ON_GUILD_SLASH_COMMAND], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_DONE);
+
+	g_Forwards[ON_GUILD_MEMBER_CREATE]				= MF_RegisterForward(g_ForwardsNames[ON_GUILD_MEMBER_CREATE], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_DONE);
+	g_Forwards[ON_GUILD_MEMBER_DELETE]				= MF_RegisterForward(g_ForwardsNames[ON_GUILD_MEMBER_DELETE], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_DONE);
+	g_Forwards[ON_GUILD_MEMBER_UPDATE]				= MF_RegisterForward(g_ForwardsNames[ON_GUILD_MEMBER_UPDATE], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_DONE);
+	g_Forwards[ON_GUILD_MEMBER_FETCH]				= MF_RegisterForward(g_ForwardsNames[ON_GUILD_MEMBER_FETCH], ET_IGNORE, FP_STRING, FP_STRING, FP_CELL, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_DONE);
+
+	g_Forwards[ON_GUILD_MEMBER_ROLE_ADD]			= MF_RegisterForward(g_ForwardsNames[ON_GUILD_MEMBER_ROLE_ADD], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_CELL, FP_DONE);
+	g_Forwards[ON_GUILD_MEMBER_ROLE_REMOVE]			= MF_RegisterForward(g_ForwardsNames[ON_GUILD_MEMBER_ROLE_REMOVE], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_STRING, FP_CELL, FP_DONE);
 
 	g_Forwards[ON_CHANNEL_MESSAGE_CREATE]			= MF_RegisterForward(g_ForwardsNames[ON_CHANNEL_MESSAGE_CREATE], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_DONE);
 }
