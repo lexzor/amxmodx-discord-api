25fe3984cb32e7e8644a1a22c282863b6bcfd256
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2026-08-20 19:53:13 +0300

    fix: build_project.ps1 does not burn my CPU

--- docker-images/debian12-i386/build_project.ps1
+++ docker-images/debian12-i386/build_project.ps1
@@ -1,11 +1,13 @@
 param(
-    [switch]$debug
+    [switch]$debug,
+    [int]$jobs = 16  # Capped at 2 to keep RAM usage under ~4GB during D++ compilation
 )
 
 $buildType = if ($debug) { "Debug" } else { "Release" }
 
 docker run -it --rm `
+    --cpu-shares 768 `
     -v "${PWD}/../..:/workspace" `
     -w /workspace `
     debian12-i386 `
-        bash -c "rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=$buildType && cores=`$(( `$(nproc) / 2 )); if [ `$cores -lt 1 ]; then cores=1; fi && make -j`$cores && echo 'Build complete ($buildType mode). Press any key to exit.' && read -n 1"
+        bash -c "rm -rf build && mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=$buildType && nice -n 10 make -j$jobs && echo 'Build complete ($buildType mode). Press any key to exit.' && read -n 1"
