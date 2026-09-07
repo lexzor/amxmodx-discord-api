4f85b2e708a4d66879822ebbfdb0e934964f938d
Nicolae Alexandru <85719945+lexzor@users.noreply.github.com>
2026-09-07 10:58:14 +0300

    feat: added OnBotLog native

--- src/amxx/amx_forwards.cpp
+++ src/amxx/amx_forwards.cpp
@@ -4,6 +4,7 @@
 const char* g_ForwardsNames[] = {
 	{ "OnBotReady" },
 	{ "OnBotStopped" },
+	{ "OnBotLog" },
 
 	{ "OnGuildCreated"},
 	{ "OnGuildDeleted"},
@@ -34,6 +35,7 @@ void RegisterForwards()
 {
 	g_Forwards[ON_BOT_READY]						= MF_RegisterForward(g_ForwardsNames[ON_BOT_READY], ET_IGNORE, FP_STRING, FP_DONE);
 	g_Forwards[ON_BOT_STOPPED]						= MF_RegisterForward(g_ForwardsNames[ON_BOT_STOPPED], ET_IGNORE, FP_STRING, FP_DONE);
+	g_Forwards[ON_BOT_LOG]							= MF_RegisterForward(g_ForwardsNames[ON_BOT_LOG], ET_IGNORE, FP_STRING, FP_CELL, FP_STRING, FP_DONE);
 
 	g_Forwards[ON_GUILD_CREATED]					= MF_RegisterForward(g_ForwardsNames[ON_GUILD_CREATED], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_DONE);
 	g_Forwards[ON_GUILD_DELETED]					= MF_RegisterForward(g_ForwardsNames[ON_GUILD_DELETED], ET_IGNORE, FP_STRING, FP_STRING, FP_STRING, FP_CELL, FP_DONE);
